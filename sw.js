32147bad905c900ba251cd2643d2163743e104d7
jlau853 <64885702+jlau853@users.noreply.github.com>
2024-09-18 13:22:03 +1200

    webmanifest test

--- sw.js
+++ sw.js
@@ -2,15 +2,10 @@ const CACHE_NAME = 'static-cache-v1';
 const resourcesToCache = [
     // ROOT FOLDER
     '/',
-    // '/index.html',
-    // '/style.css',
-    // '/script.js',
-    // '/manifest.json',
-    
     '/GractionCamp2024/index.html',
     '/GractionCamp2024/style.css',
     '/GractionCamp2024/script.js',
-    '/GractionCamp2024/manifest.json',
+    '/GractionCamp2024/manifest.webmanifest',
 
     // TO BE CHANGEd ===========================================================
     // IMAGES
@@ -44,41 +39,27 @@ const resourcesToCache = [
     '/GractionCamp2024/Teams/Blue/blue.css',
     '/GractionCamp2024/Teams/Blue/blue.html',
 
-    // '/Teams/Blue/blue.css',
-    // '/Teams/Blue/blue.html',
 
 
     '/GractionCamp2024/Teams/Green/green.css',
     '/GractionCamp2024/Teams/Green/green.html',
-
-    // '/Teams/Green/green.css',
-    // '/Teams/Green/green.html',
 
 
 
     '/GractionCamp2024/Teams/Orange/orange.css',
     '/GractionCamp2024/Teams/Orange/orange.html',
 
-    // '/Teams/Orange/orange.css',
-    // '/Teams/Orange/orange.html',
-
-
 
     '/GractionCamp2024/Teams/Purple/purple.css',
     '/GractionCamp2024/Teams/Purple/purple.html',
 
-    // '/Teams/Purple/purple.css',
-    // '/Teams/Purple/purple.html',
-    
 
 
     '/GractionCamp2024/Teams/Yellow/yellow.css',
     '/GractionCamp2024/Teams/Yellow/yellow.html',
     '/GractionCamp2024/Teams/Yellow/yellowclue1.html',
 
-    // '/Teams/Yellow/yellow.css',
-    // '/Teams/Yellow/yellow.html',
-    // '/Teams/Yellow/yellowclue1.html',
+
 ];
 
 self.addEventListener('install', event => {
@@ -116,7 +97,6 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
             return response || fetch(event.request).then(fetchResponse => {
-                // Optional: Add the response to the cache if it's not already cached
                 return caches.open(CACHE_NAME).then(cache => {
                     cache.put(event.request, fetchResponse.clone());
                     return fetchResponse;
