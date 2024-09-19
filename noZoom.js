65be9edaf7d1e0a65ddb989d45d95fd0dce6d52b
jlau853 <64885702+jlau853@users.noreply.github.com>
2024-09-19 23:46:46 +1200

    2.0.0.v9

---
+++ noZoom.js
@@ -0,0 +1,27 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Disable zooming with multiple touches
+    document.addEventListener(
+        "touchstart",
+        function (e) {
+            if (e.touches.length > 1) {
+                e.preventDefault();
+            }
+        },
+        { passive: false }
+    );
+  
+    // Disable gesture zoom
+    document.addEventListener(
+        "gesturestart",
+        function (e) {
+            e.preventDefault();
+        },
+        { passive: false }
+    );
+  });
+  
+  document.addEventListener("gesturestart", function (e) {
+    e.preventDefault();
+  });
+  
