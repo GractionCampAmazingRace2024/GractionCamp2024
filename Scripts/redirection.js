688844b410123615a62ff304bf36770f9e7a98e0
jlau853 <64885702+jlau853@users.noreply.github.com>
2024-09-21 23:39:59 +1200

    test

--- Scripts/redirection.js
+++ Scripts/redirection.js
@@ -1,13 +1,6 @@
 function returnToIndex() {
-  const themeMusic = document.getElementById('themeMusic');
-  themeMusic.play().then(() => {
-      // Wait for the music to start playing, then redirect
-      setTimeout(() => {
-          window.location.replace("/GractionCamp2024/homepage.html");
-      }, 1000); // Adjust the timeout duration as needed
-  }).catch(error => {
-      console.error("Failed to play music:", error);
-      // Redirect immediately if playback fails
-      window.location.replace("/GractionCamp2024/homepage.html");
-  });
-}
+
+  document.getElementById('themeMusic').play();
+  window.location.replace("/GractionCamp2024/homepage.html");
+
+};
