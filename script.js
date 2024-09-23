ce6887d39cdcf44777a4eac36049ae2a0edaa756
jlau853 <64885702+jlau853@users.noreply.github.com>
2024-09-23 22:45:52 +1200

    Update script.js

--- script.js
+++ script.js
@@ -283,7 +283,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 300);
   }
   loadingScreen()
-  preloadRickRoll()
+  // preloadRickRoll()
 });
 
 // Shows the indexPage and closes off the loading page and starting the theme musc
@@ -297,7 +297,7 @@ function homePage() {
   document.body.style.backgroundColor = "yellow";
   document.documentElement.style.backgroundColor = "yellow";
   playThemeMusic("themeMusic");
-  
+  preloadRickRoll()
 
 }
 
