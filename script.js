40813b3ac3a08bb04645bf0f869152d6a9c2bd92
jlau853 <64885702+jlau853@users.noreply.github.com>
2024-09-24 02:34:09 +1200

    styling and hide admin rr

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const userCredentials = {
     { password: "Gr33nG!ant", page: "c6", css: "CSS/green.css" },
     { password: "LushV3rd@nt", page: "c1", css: "CSS/green.css" },
     { password: "Spr!ngL3@ps", page: "c8", css: "CSS/green.css" },
-    { password: "JONO", page: "c10", css: "CSS/green.css" },  // Added
+    { password: "JONO", page: "c10", css: "CSS/green.css" }, // Added
   ],
   purple: [
     { password: "Purpl3V!0l3t", page: "c5", css: "CSS/purple.css" },
@@ -21,7 +21,7 @@ const userCredentials = {
     { password: "V!0l3tM!st", page: "c1", css: "CSS/purple.css" },
     { password: "Purpl3P@th", page: "c10", css: "CSS/purple.css" },
     { password: "Plum3cst@sy", page: "c6", css: "CSS/purple.css" },
-    { password: "PRIYA", page: "c9", css: "CSS/purple.css" },  // Added
+    { password: "PRIYA", page: "c9", css: "CSS/purple.css" }, // Added
   ],
   blue: [
     { password: "Blu3T!d3", page: "c7", css: "CSS/blue.css" },
@@ -33,7 +33,7 @@ const userCredentials = {
     { password: "BluM00n", page: "c5", css: "CSS/blue.css" },
     { password: "Blu3Sk!es", page: "c2", css: "CSS/blue.css" },
     { password: "D33pS3@V3ntur3", page: "c8", css: "CSS/blue.css" },
-    { password: "ASHLYN", page: "c1", css: "CSS/blue.css" },  // Added
+    { password: "ASHLYN", page: "c1", css: "CSS/blue.css" }, // Added
   ],
   orange: [
     { password: "0r@ng3Sunr!s3", page: "c3", css: "CSS/orange.css" },
@@ -45,7 +45,7 @@ const userCredentials = {
     { password: "M@ndr!nG!ng3r", page: "c5", css: "CSS/orange.css" },
     { password: "Bl@z!ngStr!d3", page: "c10", css: "CSS/orange.css" },
     { password: "0r@ng3R@d!@nc3", page: "c4", css: "CSS/orange.css" },
-    { password: "JAMES", page: "c8", css: "CSS/orange.css" },  // Added
+    { password: "JAMES", page: "c8", css: "CSS/orange.css" }, // Added
   ],
   yellow: [
     { password: "Y3ll0wL!ght", page: "c4", css: "CSS/yellow.css" },
@@ -57,12 +57,12 @@ const userCredentials = {
     { password: "Saffr0nFl@r3", page: "c8", css: "CSS/yellow.css" },
     { password: "M@r!g0ldSpr!nkl3s", page: "c7", css: "CSS/yellow.css" },
     { password: "M!dD@ySh!n3", page: "c3", css: "CSS/yellow.css" },
-    { password: "CIARA", page: "c10", css: "CSS/yellow.css" },  // Added
+    { password: "CIARA", page: "c10", css: "CSS/yellow.css" }, // Added
   ],
 
   admin: [
-    { password: "admin", page: "adminPage", css: "CSS/admin.css" },
-    { password: "rrtime", page: "rickRollPage", css: "RickRolled/rickRoll.css" },
+    { password: "settings", page: "adminPage", css: "CSS/admin.css" },
+    { password: "rrtime", page: "rickRollPage", css: "CSS/admin.csss" },
   ],
 };
 
@@ -125,134 +125,14 @@ const teamThemeMusic = {
 
 let currentThemeMusicId = ""; // Variable to store the currently playing theme music
 
+
+
+
 function getRandomErrorMessage() {
   const randomIndex = Math.floor(Math.random() * errorMessages.length);
   return errorMessages[randomIndex];
 }
 
-// Show/hide dropdown on button click
-document
-  .querySelector(".customButton")
-  .addEventListener("click", function (event) {
-    event.stopPropagation(); // Prevent event from propagating to the document
-    const dropdown = document.querySelector(".dropdown-content");
-    dropdown.classList.toggle("show");
-    // Close other dropdowns if open
-    document.querySelectorAll(".dropdown-content").forEach((otherDropdown) => {
-      if (
-        otherDropdown !== dropdown &&
-        otherDropdown.classList.contains("show")
-      ) {
-        otherDropdown.classList.remove("show");
-      }
-    });
-    document.getElementById("errorMessage").textContent = "";
-  });
-
-// Close dropdown if clicked outside
-document.addEventListener("click", function (event) {
-  if (!event.target.matches(".customButton")) {
-    document.querySelectorAll(".dropdown-content.show").forEach((dropdown) => {
-      dropdown.classList.remove("show");
-    });
-    document.getElementById("errorMessage").textContent = "";
-  }
-});
-
-// Update team selection and button style on dropdown item click
-document.querySelectorAll(".dropdown-item").forEach((item) => {
-  item.addEventListener("click", function () {
-    const value = this.getAttribute("data-value");
-    document.querySelector("#teamSelection").value = value;
-    document.querySelector(".customButton").textContent = this.textContent;
-    const bgColor = getComputedStyle(this).backgroundColor;
-    document.querySelector(".customButton").style.backgroundColor = bgColor;
-    const color = teamColors[value];
-    if (color) {
-      const loginContainer = document.querySelector(".login-container");
-      loginContainer.style.animation = "none"; // Stop the animation
-      loginContainer.style.borderColor = color;
-      loginContainer.style.boxShadow = `0 0 30px ${color}`;
-      document.getElementById(
-        "gradient-submit"
-      ).style.background = `linear-gradient(to bottom, ${color} 20%, black 20%, black 80%, ${color} 80%)`;
-      document.querySelector(
-        ".mainBody"
-      ).style.background = `linear-gradient(to bottom, ${color} 15%, black 15%, black 85%, ${color} 85%)`;
-      document.body.style.backgroundColor = color;
-
-      const colorTheme = `${[value]}Theme`;
-      console.log("Color Theme is:", colorTheme);
-      playThemeMusic(colorTheme, `${teamThemeMusic[value]}`);
-      console.log("Color Time Should be:", `${teamThemeMusic[value]}`);
-    }
-    document.querySelector(".dropdown-content").classList.remove("show");
-    document.getElementById("errorMessage").textContent = "";
-  });
-});
-
-// Handle login form submission
-document.getElementById("loginForm").addEventListener("submit", function (e) {
-  e.preventDefault(); // Prevent form from submitting automatically
-  const username = document.getElementById("teamSelection").value;
-  const password = document.getElementById("password").value;
-  document.getElementById("errorMessage").textContent = "";
-  if (!username) {
-    document.getElementById("errorMessage").textContent =
-      "Please select a team first.";
-    return;
-  }
-  if (!password) {
-    document.getElementById("errorMessage").textContent =
-      "Please enter your password.";
-    return;
-  }
-  if (userCredentials[username]) {
-    const credentials = userCredentials[username];
-    const userData = credentials.find((cred) => cred.password === password);
-
-    if (userData) {
-      sessionStorage.setItem("authenticatedUser", username);
-      sessionStorage.setItem("authenticatedPassword", password);
-
-      document
-        .querySelectorAll(".page")
-        .forEach((page) => (page.style.display = "none"));
-      document.getElementById(userData.page).style.display = "block";
-      playThemeMusic("unlockSound");
-
-      document.getElementById("iphoneline").style.display = "none";
-      const linkElement = document.getElementById("dynamic-css");
-      linkElement.href = `/GractionCamp2024/${userData.css}`; // Corrected here
-      console.log("User Data / CSS is: ", userData.css); // Access the css property from userData
-      console.log(`/GractionCamp2024/CSS/${userData.css}`);
-    } else {
-      document.getElementById("errorMessage").textContent =
-        getRandomErrorMessage();
-
-      // Play fail sound
-      const failSound = document.getElementById("failSound");
-      failSound.volume = 0.3;
-      failSound.play().catch((error) => {
-        console.log("Error playing fail sound:", error);
-      });
-
-      failSound.addEventListener("ended", () => {
-        if (currentThemeMusicId) {
-          const currentMusicElement =
-            document.getElementById(currentThemeMusicId);
-          if (currentMusicElement) {
-            currentMusicElement.volume = 1; // Restore to original volume
-          }
-        }
-      });
-    }
-  } else {
-    // Show error message if the username is invalid
-    document.getElementById("errorMessage").textContent =
-      getRandomErrorMessage();
-  }
-});
 
 // Returns each teams page to the index and starts a theme music
 function returnToIndex() {
@@ -275,7 +155,13 @@ function returnToIndex() {
   document.getElementById("errorMessage").textContent = "";
 
   // Play random theme music
-  const themeMusicOptions = ["themeMusic", "themeMusic1", "themeMusic2", 'miiTheme', 'miiShop'];
+  const themeMusicOptions = [
+    "themeMusic",
+    "themeMusic1",
+    "themeMusic2",
+    "miiTheme",
+    "miiShop",
+  ];
   const randomThemeMusicElement =
     themeMusicOptions[Math.floor(Math.random() * themeMusicOptions.length)];
   playThemeMusic(randomThemeMusicElement);
@@ -315,7 +201,7 @@ function playThemeMusic(audioFile, startTime = 0) {
 
 // Rick Roll Video
 function preloadRickRoll() {
-  playThemeMusic('miiTheme');
+  playThemeMusic("miiTheme");
   const video = document.getElementById("rickRollVideo");
   const startTime = performance.now();
   video.style.display = "none"; // Hide the video from the view
@@ -336,9 +222,7 @@ function preloadRickRoll() {
       const loaded = buffered.end(0); // Get how much of the video is buffered
       console.log(`Buffered: ${((loaded / duration) * 100).toFixed(2)}%`);
 
-      button.innerHTML = `${((loaded / duration) * 100).toFixed(2)}% Loaded`
-
-
+      button.innerHTML = `${((loaded / duration) * 100).toFixed(2)}% Loaded`;
 
       if (loaded >= duration) {
         video.pause(); // Pause the video after it's fully buffered
@@ -347,19 +231,14 @@ function preloadRickRoll() {
 
         const endTime = performance.now();
         const loadTime = (endTime - startTime) / 1000;
-        console.log(
-          `Preloading completed in ${loadTime.toFixed(2)} seconds`
-        );
+        console.log(`Preloading completed in ${loadTime.toFixed(2)} seconds`);
 
         button.onclick = homePage;
         button.disabled = false;
-        button.innerHTML = 'Press To Start<span>&#127884;</span>';
-        
+        button.innerHTML = "Press To Start<span>&#127884;</span>";
 
         // document.getElementById("playButton").style.display = "inline"; // Show the play button
         clearInterval(checkBuffering); // Stop checking when the video is fully buffered
-
-      
       }
     }
   }, 100); // Check every 500 milliseconds
@@ -367,6 +246,136 @@ function preloadRickRoll() {
 
 function playRickRoll() {
   const video = document.getElementById("rickRollVideo");
-  video.style.display = "block"; // Optionally make it visible
-  video.play(); // Play the video
+  document.getElementById("playButton").style.display = "none";
+  video.style.display = "block";
+  video.play();
+  video.addEventListener("ended", function () {
+  document.getElementById("returnToIndex").onclick = returnToIndex();
+  video.style.display = "none";
+  document.getElementById("resultsContent").innerHTML = 'placeholder result text'//`Total Time Taken: ${placeholder}`;
+
+  });
 }
+
+
+// Show/hide dropdown on button click
+document.querySelector(".customButton").addEventListener("click", function (event) {
+  event.stopPropagation(); // Prevent event from propagating to the document
+  const dropdown = document.querySelector(".dropdown-content");
+  dropdown.classList.toggle("show");
+  // Close other dropdowns if open
+  document.querySelectorAll(".dropdown-content").forEach((otherDropdown) => {
+    if (
+      otherDropdown !== dropdown &&
+      otherDropdown.classList.contains("show")
+    ) {
+      otherDropdown.classList.remove("show");
+    }
+  });
+  document.getElementById("errorMessage").textContent = "";
+});
+
+// Close dropdown if clicked outside
+document.addEventListener("click", function (event) {
+if (!event.target.matches(".customButton")) {
+  document.querySelectorAll(".dropdown-content.show").forEach((dropdown) => {
+    dropdown.classList.remove("show");
+  });
+  document.getElementById("errorMessage").textContent = "";
+}
+});
+
+// Update team selection and button style on dropdown item click
+document.querySelectorAll(".dropdown-item").forEach((item) => {
+item.addEventListener("click", function () {
+  const value = this.getAttribute("data-value");
+  document.querySelector("#teamSelection").value = value;
+  document.querySelector(".customButton").textContent = this.textContent;
+  const bgColor = getComputedStyle(this).backgroundColor;
+  document.querySelector(".customButton").style.backgroundColor = bgColor;
+  const color = teamColors[value];
+  if (color) {
+    const loginContainer = document.querySelector(".login-container");
+    loginContainer.style.animation = "none"; // Stop the animation
+    loginContainer.style.borderColor = color;
+    loginContainer.style.boxShadow = `0 0 30px ${color}`;
+    document.getElementById(
+      "gradient-submit"
+    ).style.background = `linear-gradient(to bottom, ${color} 20%, black 20%, black 80%, ${color} 80%)`;
+    document.querySelector(
+      ".mainBody"
+    ).style.background = `linear-gradient(to bottom, ${color} 15%, black 15%, black 85%, ${color} 85%)`;
+    document.body.style.backgroundColor = color;
+
+    const colorTheme = `${[value]}Theme`;
+    console.log("Color Theme is:", colorTheme);
+    playThemeMusic(colorTheme, `${teamThemeMusic[value]}`);
+    console.log("Color Time Should be:", `${teamThemeMusic[value]}`);
+  }
+  document.querySelector(".dropdown-content").classList.remove("show");
+  document.getElementById("errorMessage").textContent = "";
+});
+});
+
+// Handle login form submission
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+e.preventDefault(); // Prevent form from submitting automatically
+const username = document.getElementById("teamSelection").value;
+const password = document.getElementById("password").value;
+document.getElementById("errorMessage").textContent = "";
+if (!username) {
+  document.getElementById("errorMessage").textContent =
+    "Please select a team first.";
+  return;
+}
+if (!password) {
+  document.getElementById("errorMessage").textContent =
+    "Please enter your password.";
+  return;
+}
+if (userCredentials[username]) {
+  const credentials = userCredentials[username];
+  const userData = credentials.find((cred) => cred.password === password);
+
+  if (userData) {
+    sessionStorage.setItem("authenticatedUser", username);
+    sessionStorage.setItem("authenticatedPassword", password);
+
+    document
+      .querySelectorAll(".page")
+      .forEach((page) => (page.style.display = "none"));
+    document.getElementById(userData.page).style.display = "block";
+    playThemeMusic("unlockSound");
+
+    document.getElementById("iphoneline").style.display = "none";
+    const linkElement = document.getElementById("dynamic-css");
+    linkElement.href = `/GractionCamp2024/${userData.css}`; // Corrected here
+    console.log("User Data / CSS is: ", userData.css); // Access the css property from userData
+    console.log(`/GractionCamp2024/CSS/${userData.css}`);
+  } else {
+    document.getElementById("errorMessage").textContent =
+      getRandomErrorMessage();
+
+    // Play fail sound
+    const failSound = document.getElementById("failSound");
+    failSound.volume = 0.3;
+    failSound.play().catch((error) => {
+      console.log("Error playing fail sound:", error);
+    });
+
+    failSound.addEventListener("ended", () => {
+      if (currentThemeMusicId) {
+        const currentMusicElement =
+          document.getElementById(currentThemeMusicId);
+        if (currentMusicElement) {
+          currentMusicElement.volume = 1; // Restore to original volume
+        }
+      }
+    });
+  }
+} else {
+  // Show error message if the username is invalid
+  document.getElementById("errorMessage").textContent =
+    getRandomErrorMessage();
+}
+});
